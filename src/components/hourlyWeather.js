393c8a29a86841912f3ad31826b97edbb04e94ac
ec20315 <ec20315@qmul.ac.uk>
2024-03-20 11:11:52 +0000

    Added groundwork for icons

--- src/components/hourlyWeather.js
+++ src/components/hourlyWeather.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useWeatherData } from './ApiManager';
 import { kelvinToCelsius } from './reusable';
+import IconCloud from '../images/icon_cloud.svg';
+import IconSun from '../images/icon_sun.svg';
+import IconRain from '../images/icon_rain.svg';
+import IconThunderstorm from '../images/icon_thunderstorm.svg';
 
 function HourlyWeatherBox({ lat, long }) {
   const { forecastData } = useWeatherData(lat, long );
@@ -15,13 +19,36 @@ function HourlyWeatherBox({ lat, long }) {
   return (
     <div className="hourly-weather-box">
       <div className="hourly-weather-list">
-        {next5HoursForecast.map((forecast, index) => (
-          <div key={index} className="hourly-weather-item">
-            <h3>{new Date(forecast.dt_txt).getHours()}:00</h3>
-            <h4>{forecast.weather[0].description}</h4>
-            <h3>{kelvinToCelsius(forecast.main.temp)}°</h3>
-          </div>
-        ))}
+        {next5HoursForecast.map((forecast, index) => {
+          const description = forecast.weather[0].description.toLowerCase();
+          let icon;
+
+          if (description.includes('overcast clouds')) {
+            icon = <img src={IconCloud} alt="Overcast Clouds" />; //maybe diff img here too?
+          } else if (description.includes('clear sky')) {
+            icon = <img src={IconSun} alt="Clear Sky" />;
+          } else if (description.includes('rain')) {
+            icon = <img src={IconRain} alt="Rain" />;
+          } else if (description.includes('shower rain')) {
+            icon = <img src={IconRain} alt="Shower Rain" />;
+          } else if (description.includes('thunderstorm')) {
+            icon = <img src={IconThunderstorm} alt="Thunderstorm" />;
+          } else if (description.includes('broken clouds')) { //I ran out of free icon imports need to replace this
+            icon = <img src={IconCloud} alt="Broken Clouds" />;
+	  } else if (description.includes('scattered clouds')) {
+            icon = <img src={IconCloud} alt="Scattered Clouds" />;
+          } else {
+            icon = <span>{description}</span>;
+          }
+
+          return (
+            <div key={index} className="hourly-weather-item">
+              <h3>{new Date(forecast.dt_txt).getHours()}:00</h3>
+              {icon}
+              <h3>{kelvinToCelsius(forecast.main.temp)}°</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/weeklyForecast.js
+++ src/components/weeklyForecast.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useWeatherData } from './ApiManager';
 import { kelvinToCelsius } from './reusable';
-
+import IconCloud from '../images/icon_cloud.svg';
+import IconSun from '../images/icon_sun.svg';
+import IconRain from '../images/icon_rain.svg';
+import IconThunderstorm from '../images/icon_thunderstorm.svg';
 
 function WeeklyForecast({ lat, long  }) {
     const { dailyForecastData } = useWeatherData(lat, long );
@@ -10,20 +13,39 @@ function WeeklyForecast({ lat, long  }) {
         return <div>Loading...</div>;
     }
 
-    return (
-        <div className="weekly-forecast-box">
-            <h1 className='forecastTitle'>Weekly Forecast</h1>
-            <div className="forecast-list">
-                {dailyForecastData.list.map((forecast, index) => (
-                    <div key={index} className="forecast-item">
-                        <h3>{new Date(forecast.dt * 1000).toLocaleDateString('en-US', { weekday: 'short' })}</h3>
-                        <p>{forecast.weather[0].description}</p> 
-                        <p>{kelvinToCelsius(forecast.temp.day)}°</p>
-                    </div>
-                ))}
+  return (
+    <div className="weekly-forecast-box">
+      <div className="forecast-list">
+        {dailyForecastData.list.map((forecast, index) => {
+          const description = forecast.weather[0].description.toLowerCase();
+          let icon;
+          if (description.includes('overcast clouds')) {
+            icon = <img src={IconCloud} alt="Overcast Clouds" />; //maybe diff img here too?
+          } else if (description.includes('clear sky')) {
+            icon = <img src={IconSun} alt="Clear Sky" />;
+          } else if (description.includes('rain')) {
+            icon = <img src={IconRain} alt="Rain" />;
+          } else if (description.includes('shower rain')) {
+            icon = <img src={IconRain} alt="Shower Rain" />;
+          } else if (description.includes('thunderstorm')) {
+            icon = <img src={IconThunderstorm} alt="Thunderstorm" />;
+          } else if (description.includes('broken clouds')) { //I ran out of free icon imports need to replace this
+            icon = <img src={IconCloud} alt="Broken Clouds" />;
+          } else {
+            icon = <span>{description}</span>;
+          }
+
+          return (
+            <div key={index} className="forecast-item">
+              <h3>{new Date(forecast.dt * 1000).toLocaleDateString('en-US', { weekday: 'short' })}</h3>
+              {icon}
+              <p>{kelvinToCelsius(forecast.temp.day)}°C</p>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default WeeklyForecast;
